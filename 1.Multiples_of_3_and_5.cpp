c5c0e6eeb3a3acd3ac33e62e339eabc603c0e27e
Markandeya <vasalpha@gmail.com>
2019-02-16 15:29:02 +0530

    Modifies Problem 1 soln

--- 1.Multiples_of_3_and_5.cpp
+++ 1.Multiples_of_3_and_5.cpp
@@ -9,10 +9,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 /*
 Logic:
-
-We need to  apply the 'Principle of Inclusion and Exclusion' to solve this problem.
-Here it simply means, we find all factors of 3 and 5 separately and add them before
-subtracting the lcm(3,5) or 15's factors from the total sum.
+Add to the sum if number is divisible by 3 or 5.
 
 */
 
@@ -23,26 +20,10 @@ using namespace std;
 int main()
 {
     int sum = 0;
-    int count_3 = 1, count_5 = 1, count_15 = 1;
+    for (int i = 0; i < 1000; ++i)
+        if (i%3==0 || i%5==0)
+            sum += i;
 
-    //loop to add 3s multiples
-    while(3*count_3 < 1000)
-    {
-        sum += 3*count_3++;
-    }
-
-    //loop to add 5s multiples
-    while(5*count_5 < 1000)
-    {
-     //   if ((5*count_5)%15 != 0)
-            sum += 5*count_5++;
-    }
-
-    //Removing 15s multiples
-    while(15*count_15 < 1000)
-    {
-        sum -= 15*count_15++;
-    }
     cout<<sum<<endl;
 
     return 0;
